51a85f2c4d3fea053b53751d14303695db9dec3b
Podgorskiy <stanislav@podgorskiy.com>
2019-03-08 00:10:21 -0500

    Initial

---
+++ LRGAN.py
@@ -0,0 +1,159 @@
+from __future__ import print_function
+import torch.utils.data
+from scipy import misc
+from torch import optim
+from torchvision.utils import save_image
+from net import *
+from torch.autograd import Variable
+import numpy as np
+import pickle
+import time
+import random
+import os
+import dlutils
+#dlutils.use_cuda = False
+from dlutils import batch_provider
+from dlutils.pytorch.cuda_helper import *
+from torch.nn import functional as F
+import math
+
+im_size = 32
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    #BCE = F.binary_cross_entropy(recon_x * 0.5 + 0.5, x * 0.5 + 0.5)
+    BCE = torch.mean((recon_x - x)**2)
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    #KLD = -0.5 * torch.mean(torch.mean(1 + logvar - mu.pow(2) - logvar.exp(), 1))
+    return BCE, 0#KLD * 0.05
+
+
+def process_batch(batch):
+    label = [x[0] for x in batch]
+    data = [misc.imresize(x[1], [im_size, im_size]).transpose((2, 0, 1)) for x in batch]
+    
+    y = torch.from_numpy(np.asarray(label)).type(LongTensor).cuda()
+    x = torch.from_numpy(np.asarray(data, dtype=np.float32)).cuda() / 127.5 - 1.
+    x = x.view(-1, 3, im_size, im_size)
+    return x, y
+
+
+def compose(f, b):
+    i = f[:, :3]
+    m = f[:, 3:4]
+    return i * m + b * (1.0 - m)
+
+
+def main(folding_id, total_classes, inliner_classes, folds=5):
+    batch_size = 128
+    z1size = 256
+    z2size = 64
+    data_train = []
+    data_valid = []
+
+    for i in range(folds):
+        if True:#i != folding_id:
+            with open('F:/DATASETS/celeba/' + 'data_fold_%d.pkl' % i, 'rb') as pkl: #'F:/DATASETS/celeba/' +
+                fold = pickle.load(pkl)
+            if False:#len(data_valid) == 0:
+                data_valid = fold
+            else:
+                data_train += fold
+
+    outlier_classes = []
+    for i in range(total_classes):
+        if i not in inliner_classes:
+            outlier_classes.append(i)
+
+    #keep only train classes
+    #data_train = [x for x in data_train if x[0] in inliner_classes]
+
+    print("Train set size:", len(data_train))
+
+    GF = VAE(zsize=z1size)
+    GF.cuda()
+    GF.train()
+    GF.weight_init(mean=0, std=0.02)
+
+    lr = 0.001
+
+    G_optimizer = optim.Adam(GF.parameters(), lr=lr, betas=(0.5, 0.999), weight_decay=1e-5)
+ 
+    train_epoch = 300
+
+    sample1 = torch.randn(128, z1size).view(-1, z1size, 1, 1)
+
+    for epoch in range(train_epoch):
+        GF.train()
+
+        random.shuffle(data_train)
+
+        batches = batch_provider(data_train, batch_size, process_batch, report_progress=True)
+
+        Rtrain_loss = 0
+        KLtrain_loss = 0
+
+        epoch_start_time = time.time()
+
+        if (epoch + 1) % 100 == 0:
+            G_optimizer.param_groups[0]['lr'] /= 4
+            print("learning rate change!")
+
+        i = 0
+        for x, y in batches:
+            GF.train()
+            x_ = x
+            GF.zero_grad()
+            rec, mu, logvar = GF(x)
+
+            loss_re, loss_kl = loss_function(rec, x, mu, logvar)
+            (loss_re ).backward()
+            G_optimizer.step()
+            Rtrain_loss += loss_re.item()
+            #KLtrain_loss += loss_kl.item()
+
+            #############################################
+
+            directory = 'results_rec'+str(inliner_classes[0])
+            os.makedirs(directory, exist_ok=True)
+            directory = 'results_gen'+str(inliner_classes[0])
+            os.makedirs(directory, exist_ok=True)
+
+
+            epoch_end_time = time.time()
+            per_epoch_ptime = epoch_end_time - epoch_start_time
+
+            m = 40
+            i += 1
+            if i % m == 0:
+                Rtrain_loss /= (m)
+                KLtrain_loss /= (m)
+                print('[%d/%d] - ptime: %.2f, Rloss: %.9f, KLloss: %.9f' % ((epoch + 1), train_epoch, per_epoch_ptime, Rtrain_loss, KLtrain_loss))
+                Rtrain_loss = 0
+                KLtrain_loss = 0
+
+                with torch.no_grad():
+                    GF.eval()
+                    x_fake, _, _ = GF(x_)
+                    #x_fake = compose(x_foreground, x_background)
+                    resultsample = torch.cat([x_, x_fake]) * 0.5 + 0.5
+                    resultsample = resultsample.cpu()
+                    save_image(resultsample.view(-1, 3, im_size, im_size), 'results_rec'+str(inliner_classes[0])+'/sample_' + str(epoch) +"_"+ str(i) + '.png')
+                    x_fake = GF.decode(sample1)
+                    #x_fake = compose(x_foreground, x_background)
+                    resultsample = x_fake * 0.5 + 0.5
+                    resultsample = resultsample.cpu()
+                    save_image(resultsample.view(-1, 3, im_size, im_size), 'results_gen'+str(inliner_classes[0])+'/sample_' + str(epoch) +"_"+ str(i) + '.png')
+                    #save_image(x_for_pred.view(-1, 3, im_size, im_size), 'results'+str(inliner_classes[0])+'/x_for_pred_' + str(epoch) + '.png')
+
+
+
+    print("Training finish!... save training results")
+    torch.save(GF.state_dict(), "VAEmodel_%d.pkl" %(folding_id))
+
+if __name__ == '__main__':
+    main(0, 10, [0])
---
+++ net.py
@@ -0,0 +1,275 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class VAE(nn.Module):
+    def __init__(self, zsize):
+        super(VAE, self).__init__()
+
+        d = 128
+        self.d = d
+        self.zsize = zsize
+        self.d1 = nn.Linear(zsize, d * 2 * 4 * 4)
+        self.deconv1_bn = nn.BatchNorm2d(d * 2)
+        self.deconv2 = nn.ConvTranspose2d(d * 2, d * 2, 4, 2, 1)
+        self.deconv2_bn = nn.BatchNorm2d(d * 2)
+        self.deconv3 = nn.ConvTranspose2d(d * 2, d, 4, 2, 1)
+        self.deconv3_bn = nn.BatchNorm2d(d)
+        self.deconv4 = nn.ConvTranspose2d(d, 3, 4, 2, 1)
+
+        self.conv1 = nn.Conv2d(3, d // 2, 4, 2, 1)
+        self.conv1_bn = nn.BatchNorm2d(d // 2)
+        self.conv2 = nn.Conv2d(d // 2, d * 2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d * 2)
+        self.conv3 = nn.Conv2d(d * 2, d * 4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d * 4)
+        self.conv4_1_bn = nn.BatchNorm2d(d * 4)
+        self.conv4_2_bn = nn.BatchNorm2d(d * 4)
+
+        self.fc1 = nn.Linear(d * 4 * 4 * 4, zsize)
+        self.fc2 = nn.Linear(d * 4 * 4 * 4, zsize)
+
+    def encode(self, x):
+        x = F.relu((self.conv1(x)), 0.0)
+        x = F.relu((self.conv2(x)), 0.0)
+        x = F.relu((self.conv3(x)), 0.0)
+        x = x.view(x.shape[0], self.d * 4 * 4 * 4)
+        h1 = self.fc1(x)
+        h2 = self.fc2(x)
+        return h1, h2
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, x):
+        x = x.view(x.shape[0], self.zsize)
+        x = self.d1(x)
+        x = x.view(x.shape[0], self.d * 2, 4, 4)
+        x = self.deconv1_bn(x)
+        x = F.relu(x, 0.0)
+        x = F.relu(self.deconv2_bn(self.deconv2(x)), 0.0)
+        x = F.relu(self.deconv3_bn(self.deconv3(x)), 0.0)
+        x = F.tanh(self.deconv4(x))# * 0.5 + 0.5
+        return x
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        mu = mu.squeeze()
+        logvar = logvar.squeeze()
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z.view(-1, self.zsize, 1, 1)), mu, logvar
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+#
+#
+# class VAE(nn.Module):
+#     def __init__(self, zsize, channels=3):
+#         super(VAE, self).__init__()
+#         d = 64
+#         self.zsize = zsize
+#
+#         layer_count = 4
+#         inputs = zsize
+#         mul = 1
+#         for i in range(layer_count):
+#             mul *= 2
+#         for i in range(layer_count - 1):
+#             setattr(self, "deconv%d" % (i + 1), nn.ConvTranspose2d(inputs, d * mul, 4, 2 if i > 0 else 1, 1 if i > 0 else 0))
+#             setattr(self, "deconv%d_bn" % (i + 1), nn.BatchNorm2d(d * mul))
+#             inputs = d * mul
+#             mul = mul // 2
+#         i = layer_count - 1
+#         setattr(self, "deconv%d" % (i + 1), nn.ConvTranspose2d(inputs, channels, 4, 2, 1))
+#
+#         mul = 1
+#         inputs = channels
+#         for i in range(layer_count-1):
+#             setattr(self, "conv%d" % (i + 1), nn.Conv2d(inputs, d * mul, 4, 2, 1))
+#             setattr(self, "conv%d_bn" % (i + 1), nn.BatchNorm2d(d * mul))
+#             inputs = d * mul
+#             mul = mul * 2
+#         i = layer_count - 1
+#         setattr(self, "conv%d_1" % (i + 1), nn.Conv2d(inputs, zsize, 4, 1, 0))
+#         setattr(self, "conv%d_2" % (i + 1), nn.Conv2d(inputs, zsize, 4, 1, 0))
+#
+#     def encode(self, x):
+#         x = F.leaky_relu((self.conv1(x)), 0.2)
+#         x = F.leaky_relu((self.conv2(x)), 0.2)
+#         x = F.leaky_relu((self.conv3(x)), 0.2)
+#
+#         h1 = self.conv4_1(x)
+#         h2 = self.conv4_2(x)
+#         return h1, h2
+#
+#     def reparameterize(self, mu, logvar):
+#         if self.training:
+#             std = torch.exp(0.5 * logvar)
+#             eps = torch.randn_like(std)
+#             return eps * std + mu
+#         else:
+#             return mu
+#
+#     def decode(self, z):
+#         x = z.view(-1, self.zsize, 1, 1)
+#         x = F.relu(self.deconv1_bn(self.deconv1(x)))
+#         x = F.relu(self.deconv2_bn(self.deconv2(x)))
+#         x = F.relu(self.deconv3_bn(self.deconv3(x)))
+#         x = F.tanh(self.deconv4(x))# * 0.5 + 0.5
+#         return x
+#
+#     def forward(self, x):
+#         mu, logvar = self.encode(x)
+#         mu = mu.squeeze()
+#         logvar = logvar.squeeze()
+#         z = self.reparameterize(mu, logvar)
+#         return self.decode(z.view(-1, self.zsize, 1, 1)), mu, logvar
+#
+#     def weight_init(self, mean, std):
+#         for m in self._modules:
+#             normal_init(self._modules[m], mean, std)
+#
+
+class DCGAN_D(nn.Module):
+    def __init__(self, isize=32, nz=100, nc=3, ndf=128, ngpu=1, n_extra_layers=0, out=1):
+        super(DCGAN_D, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        main = nn.Sequential()
+        # input is nc x isize x isize
+        main.add_module('initial_conv_{0}-{1}'.format(nc, ndf),
+                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=True))
+        main.add_module('initial_relu_{0}'.format(ndf),
+                        nn.LeakyReLU(0.2, inplace=True))
+        csize, cndf = isize / 2, ndf
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}_{1}_conv'.format(t, cndf),
+                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}_{1}_batchnorm'.format(t, cndf),
+                            nn.BatchNorm2d(cndf))
+                            #nn.InstanceNorm2d(cndf, affine=True))
+            main.add_module('extra-layers-{0}_{1}_relu'.format(t, cndf),
+                            nn.LeakyReLU(0.2, inplace=True))
+
+        while csize > 4:
+            in_feat = cndf
+            out_feat = cndf * 2
+            main.add_module('pyramid_{0}-{1}_conv'.format(in_feat, out_feat),
+                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+            main.add_module('pyramid_{0}_batchnorm'.format(out_feat),
+                            nn.BatchNorm2d(out_feat))
+                            #nn.InstanceNorm2d(out_feat, affine=True))
+            main.add_module('pyramid_{0}_relu'.format(out_feat),
+                            nn.LeakyReLU(0.2, inplace=True))
+            cndf = cndf * 2
+            csize = csize / 2
+
+        # state size. K x 4 x 4
+        main.add_module('final_{0}-{1}_conv'.format(cndf, 1),
+                        nn.Conv2d(cndf, out, 4, 1, 0, bias=False))
+        self.main = main
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        return output
+
+
+class DCGAN_G(nn.Module):
+    def __init__(self, isize=32, nz=100, nc=3, ngf=64, ngpu=1, n_extra_layers=0):
+        super(DCGAN_G, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        cngf, tisize = ngf // 2, 4
+        while tisize != isize:
+            cngf = cngf * 2
+            tisize = tisize * 2
+
+        main = nn.Sequential()
+        # input is Z, going into a convolution
+        main.add_module('initial_{0}-{1}_convt'.format(nz, cngf),
+                        nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
+        main.add_module('initial_{0}_batchnorm'.format(cngf),
+                        nn.BatchNorm2d(cngf))
+        main.add_module('initial_{0}_relu'.format(cngf),
+                        nn.ReLU(True))
+
+        csize, cndf = 4, cngf
+        while csize < isize // 2:
+            main.add_module('pyramid_{0}-{1}_convt'.format(cngf, cngf // 2),
+                            nn.ConvTranspose2d(cngf, cngf // 2, 4, 2, 1, bias=False))
+            main.add_module('pyramid_{0}_batchnorm'.format(cngf // 2),
+                            nn.BatchNorm2d(cngf // 2))
+            main.add_module('pyramid_{0}_relu'.format(cngf // 2),
+                            nn.ReLU(True))
+            cngf = cngf // 2
+            csize = csize * 2
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}_{1}_conv'.format(t, cngf),
+                            nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}_{1}_batchnorm'.format(t, cngf),
+                            nn.BatchNorm2d(cngf))
+            main.add_module('extra-layers-{0}_{1}_relu'.format(t, cngf),
+                            nn.ReLU(True))
+
+        main.add_module('final_{0}-{1}_convt'.format(cngf, nc),
+                        nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=True))
+        main.add_module('final_{0}_tanh'.format(nc),
+                        nn.Tanh())
+        self.main = main
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)# * 0.5 + 0.5
+        return output
+
+
+class DCGAN_T(nn.Module):
+    def __init__(self, isize=32, nz=128, nc_in=3, nc_out=3, nf=64):
+        super(DCGAN_T, self).__init__()
+        self.g = DCGAN_G(isize, nz, nc_out, nf)
+        self.d = DCGAN_D(isize, nz, nc_in, nf, out=nz)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        output = self.g(self.d(input))
+        return output
+
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
