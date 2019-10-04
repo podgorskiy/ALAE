b94c1e75dcf5714f8065552b6ecf3510d8aad3ca
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2019-10-04 03:38:40 -0400

    Updated lreq

--- lreq.py
+++ lreq.py
@@ -21,6 +21,9 @@ from torch.nn.parameter import Parameter
 import numpy as np
 
 
+use_implicit_lreq = True
+
+
 def is_sequence(arg):
     return (not hasattr(arg, "strip") and
             hasattr(arg, "__getitem__") or
@@ -34,7 +37,7 @@ def make_tuple(x, n):
 
 
 class Linear(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2))):
+    def __init__(self, in_features, out_features, bias=True, gain=np.sqrt(2.0), lrmul=1.0, implicit_lreq=use_implicit_lreq):
         super(Linear, self).__init__()
         self.in_features = in_features
         self.weight = Parameter(torch.Tensor(out_features, in_features))
@@ -44,22 +47,38 @@ class Linear(nn.Module):
             self.register_parameter('bias', None)
         self.std = 0
         self.gain = gain
+        self.lrmul = lrmul
+        self.implicit_lreq = implicit_lreq
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.std = self.gain / np.sqrt(self.in_features)
-        init.normal_(self.weight)#, std=self.std)
+        self.std = self.gain / np.sqrt(self.in_features) * self.lrmul
+        if not self.implicit_lreq:
+            init.normal_(self.weight, mean=0, std=1.0 / self.lrmul)
+        else:
+            init.normal_(self.weight, mean=0, std=self.std / self.lrmul)
+            setattr(self.weight, 'lr_equalization_coef', self.std)
+            if self.bias is not None:
+                setattr(self.bias, 'lr_equalization_coef', self.lrmul)
+
         if self.bias is not None:
             with torch.no_grad():
                 self.bias.zero_()
 
     def forward(self, input):
-        return F.linear(input, self.weight * self.std, self.bias)
+        if not self.implicit_lreq:
+            bias = self.bias
+            if bias is not None:
+                bias = bias * self.lrmul
+            return F.linear(input, self.weight * self.std, bias)
+        else:
+            return F.linear(input, self.weight, self.bias)
 
 
 class Conv2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
-                 groups=1, bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2)), transpose=False):
+                 groups=1, bias=True, gain=np.sqrt(2.0), transpose=False, transform_kernel=False, lrmul=1.0,
+                 implicit_lreq=use_implicit_lreq):
         super(Conv2d, self).__init__()
         if in_channels % groups != 0:
             raise ValueError('in_channels must be divisible by groups')
@@ -74,7 +93,10 @@ class Conv2d(nn.Module):
         self.dilation = make_tuple(dilation, 2)
         self.groups = groups
         self.gain = gain
+        self.lrmul = lrmul
         self.transpose = transpose
+        self.fan_in = np.prod(self.kernel_size) * in_channels // groups
+        self.transform_kernel = transform_kernel
         if transpose:
             self.weight = Parameter(torch.Tensor(in_channels, out_channels // groups, *self.kernel_size))
         else:
@@ -84,35 +106,78 @@ class Conv2d(nn.Module):
         else:
             self.register_parameter('bias', None)
         self.std = 0
-        self.fan_in = np.prod(kernel_size) * in_channels
+        self.implicit_lreq = implicit_lreq
         self.reset_parameters()
 
     def reset_parameters(self):
         self.std = self.gain / np.sqrt(self.fan_in)
-        init.normal_(self.weight)#, std=self.std)
+        if not self.implicit_lreq:
+            init.normal_(self.weight, mean=0, std=1.0 / self.lrmul)
+        else:
+            init.normal_(self.weight, mean=0, std=self.std / self.lrmul)
+            setattr(self.weight, 'lr_equalization_coef', self.std)
+            if self.bias is not None:
+                setattr(self.bias, 'lr_equalization_coef', self.lrmul)
+
         if self.bias is not None:
             with torch.no_grad():
                 self.bias.zero_()
 
     def forward(self, x):
         if self.transpose:
-            return F.conv_transpose2d(x, self.weight * self.std, self.bias, stride=self.stride, padding=self.padding,
-                                      output_padding=self.output_padding, dilation=self.dilation, groups=self.groups)
+            w = self.weight
+            if self.transform_kernel:
+                w = F.pad(w, (1, 1, 1, 1), mode='constant')
+                w = w[:, :, 1:, 1:] + w[:, :, :-1, 1:] + w[:, :, 1:, :-1] + w[:, :, :-1, :-1]
+            if not self.implicit_lreq:
+                bias = self.bias
+                if bias is not None:
+                    bias = bias * self.lrmul
+                return F.conv_transpose2d(x, w * self.std, bias, stride=self.stride,
+                                          padding=self.padding, output_padding=self.output_padding,
+                                          dilation=self.dilation, groups=self.groups)
+            else:
+                return F.conv_transpose2d(x, w, self.bias, stride=self.stride, padding=self.padding,
+                                          output_padding=self.output_padding, dilation=self.dilation,
+                                          groups=self.groups)
         else:
-            return F.conv2d(x, self.weight * self.std, self.bias, stride=self.stride, padding=self.padding,
-                            dilation=self.dilation, groups=self.groups)
+            w = self.weight
+            if self.transform_kernel:
+                w = F.pad(w, (1, 1, 1, 1), mode='constant')
+                w = (w[:, :, 1:, 1:] + w[:, :, :-1, 1:] + w[:, :, 1:, :-1] + w[:, :, :-1, :-1]) * 0.25
+            if not self.implicit_lreq:
+                bias = self.bias
+                if bias is not None:
+                    bias = bias * self.lrmul
+                return F.conv2d(x, w * self.std, bias, stride=self.stride, padding=self.padding,
+                                dilation=self.dilation, groups=self.groups)
+            else:
+                return F.conv2d(x, w, self.bias, stride=self.stride, padding=self.padding,
+                                dilation=self.dilation, groups=self.groups)
 
 
 class ConvTranspose2d(Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
-                 groups=1, bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2))):
-        super(ConvTranspose2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding,
-                                              output_padding, dilation, groups, bias, gain, True)
+                 groups=1, bias=True, gain=np.sqrt(2.0), transform_kernel=False, lrmul=1.0, implicit_lreq=use_implicit_lreq):
+        super(ConvTranspose2d, self).__init__(in_channels=in_channels,
+                                              out_channels=out_channels,
+                                              kernel_size=kernel_size,
+                                              stride=stride,
+                                              padding=padding,
+                                              output_padding=output_padding,
+                                              dilation=dilation,
+                                              groups=groups,
+                                              bias=bias,
+                                              gain=gain,
+                                              transpose=True,
+                                              transform_kernel=transform_kernel,
+                                              lrmul=lrmul,
+                                              implicit_lreq=implicit_lreq)
 
 
 class SeparableConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
-                 bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2)), transpose=False):
+                 bias=True, gain=np.sqrt(2.0), transpose=False):
         super(SeparableConv2d, self).__init__()
         self.spatial_conv = Conv2d(in_channels, in_channels, kernel_size, stride, padding, output_padding, dilation,
                                    in_channels, False, 1, transpose)
@@ -124,7 +189,7 @@ class SeparableConv2d(nn.Module):
 
 class SeparableConvTranspose2d(Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
-                 bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2))):
+                 bias=True, gain=np.sqrt(2.0)):
         super(SeparableConvTranspose2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding,
                                               output_padding, dilation, bias, gain, True)
 
