179ebed9a2359f0d507633257758575d27b2193f
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2019-11-22 09:58:44 -0500

    Generations

---
+++ ls_save_images.py
@@ -0,0 +1,159 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+"""Linear Separability (LS)."""
+import tensorflow as tf
+import torch
+import dnnlib
+import dnnlib.tflib
+import dnnlib.tflib as tflib
+import pickle
+from dataloader import *
+import scipy.linalg
+
+from checkpointer import Checkpointer
+from scheduler import ComboMultiStepLR
+
+from dlutils import batch_provider
+from dlutils.pytorch.cuda_helper import *
+from dlutils.pytorch import count_parameters
+from defaults import get_cfg_defaults
+from model_z_gan import Model
+import argparse
+import logging
+import sys
+import lreq
+from skimage.transform import resize
+from tqdm import tqdm
+
+from launcher import run
+from PIL import Image
+from matplotlib import pyplot as plt
+import utils
+from net import *
+
+from collections import defaultdict
+import numpy as np
+import sklearn.svm
+import tensorflow as tf
+import dnnlib.tflib as tflib
+
+from metrics import metric_base
+from training import misc
+
+dnnlib.tflib.init_tf()
+tf_config     = {'rnd.np_random_seed': 1000}
+
+
+class LS:
+    def __init__(self, cfg, num_samples, num_keep, attrib_indices, minibatch_gpu):
+        assert num_keep <= num_samples
+        self.num_samples = num_samples
+        self.num_keep = num_keep
+        self.attrib_indices = attrib_indices
+        self.minibatch_size = minibatch_gpu
+        self.cfg = cfg
+
+    def evaluate(self, logger, mapping, decoder, lod):
+        tfr_opt = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.NONE)
+        tfr_writer = tf.python_io.TFRecordWriter("generated_data.000", tfr_opt)
+        # Sampling loop.
+
+        rnd = np.random.RandomState(5)
+
+        for _ in tqdm(range(0, self.num_samples, self.minibatch_size)):
+            # Generate images.
+            torch.cuda.set_device(0)
+            latents = rnd.randn(self.minibatch_size, self.cfg.MODEL.LATENT_SPACE_SIZE)
+            lat = torch.tensor(latents).float().cuda()
+
+            dlat = mapping(lat)
+            images = decoder(dlat, lod, 1.0, True)
+
+            # Downsample to 256x256. The attribute classifiers were built for 256x256.
+            if images.shape[2] > 256:
+                factor = images.shape[2] // 256
+                images = torch.reshape(images,
+                                    [-1, images.shape[1], images.shape[2] // factor, factor, images.shape[3] // factor,
+                                     factor])
+                images = torch.mean(images, dim=(3, 5))
+            images = np.clip((images.cpu().numpy() + 1.0) * 127, 0, 255).astype(np.uint8)
+
+            for img in images:
+                ex = tf.train.Example(features=tf.train.Features(feature={
+                    'shape': tf.train.Feature(int64_list=tf.train.Int64List(value=img.shape)),
+                    'data': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img.tostring()]))}))
+                tfr_writer.write(ex.SerializeToString())
+
+
+def sample(cfg, logger):
+    torch.cuda.set_device(0)
+    model = Model(
+        startf=cfg.MODEL.START_CHANNEL_COUNT,
+        layer_count=cfg.MODEL.LAYER_COUNT,
+        maxf=cfg.MODEL.MAX_CHANNEL_COUNT,
+        latent_size=cfg.MODEL.LATENT_SPACE_SIZE,
+        truncation_psi=cfg.MODEL.TRUNCATIOM_PSI,
+        truncation_cutoff=cfg.MODEL.TRUNCATIOM_CUTOFF,
+        mapping_layers=cfg.MODEL.MAPPING_LAYERS,
+        channels=cfg.MODEL.CHANNELS,
+        generator=cfg.MODEL.GENERATOR,
+        encoder=cfg.MODEL.ENCODER)
+
+    model.cuda()
+    model.eval()
+    model.requires_grad_(False)
+
+    decoder = model.decoder
+    encoder = model.encoder
+    mapping_tl = model.mapping_tl
+    mapping_fl = model.mapping_fl
+    dlatent_avg = model.dlatent_avg
+
+    logger.info("Trainable parameters generator:")
+    count_parameters(decoder)
+
+    logger.info("Trainable parameters discriminator:")
+    count_parameters(encoder)
+
+    arguments = dict()
+    arguments["iteration"] = 0
+
+    model_dict = {
+        'discriminator_s': encoder,
+        'generator_s': decoder,
+        'mapping_tl_s': mapping_tl,
+        'mapping_fl_s': mapping_fl,
+        'dlatent_avg': dlatent_avg
+    }
+
+    checkpointer = Checkpointer(cfg,
+                                model_dict,
+                                {},
+                                logger=logger,
+                                save=False)
+
+    checkpointer.load()
+
+    model.eval()
+
+    layer_count = cfg.MODEL.LAYER_COUNT
+
+    logger.info("Evaluating LS metric")
+
+    decoder = nn.DataParallel(decoder)
+    mapping_fl = nn.DataParallel(mapping_fl)
+
+    with torch.no_grad():
+        ppl = LS(cfg, num_samples=50000, num_keep=10000, attrib_indices=range(40), minibatch_gpu=8)
+        ppl.evaluate(logger, mapping_fl, decoder, cfg.DATASET.MAX_RESOLUTION_LEVEL - 2)
+
+
+if __name__ == "__main__":
+    gpu_count = 1
+    run(sample, get_cfg_defaults(), description='StyleGAN', default_config='configs/experiment_ffhq_z.yaml',
+        world_size=gpu_count, write_log=False)
---
+++ make_recon_figure_short_sep.py
@@ -0,0 +1,190 @@
+# Copyright 2019 Stanislav Pidhorskyi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import print_function
+import torch.utils.data
+from scipy import misc
+from torch import optim
+from torchvision.utils import save_image
+from net import *
+import numpy as np
+import pickle
+import time
+import random
+import os
+from model import Model
+from net import *
+from checkpointer import Checkpointer
+from scheduler import ComboMultiStepLR
+# from model_z_gan import Model
+from model_z_gan_sep import Model
+from launcher import run
+from defaults import get_cfg_defaults
+import lod_driver
+
+
+from checkpointer import Checkpointer
+from scheduler import ComboMultiStepLR
+
+from dlutils import batch_provider
+from dlutils.pytorch.cuda_helper import *
+from dlutils.pytorch import count_parameters
+from defaults import get_cfg_defaults
+import argparse
+import logging
+import sys
+import bimpy
+import lreq
+from skimage.transform import resize
+import utils
+
+from PIL import Image
+
+
+lreq.use_implicit_lreq.set(True)
+
+im_size = 128
+
+
+def place(canvas, image, x, y):
+    im_size = image.shape[2]
+    if len(image.shape) == 4:
+        image = image[0]
+    canvas[:, y: y + im_size, x: x + im_size] = image * 0.5 + 0.5
+
+
+def save_sample(model, sample, i):
+    os.makedirs('results', exist_ok=True)
+
+    with torch.no_grad():
+        model.eval()
+        x_rec = model.generate(model.generator.layer_count - 1, 1, z=sample)
+
+        def save_pic(x_rec):
+            resultsample = x_rec * 0.5 + 0.5
+            resultsample = resultsample.cpu()
+            save_image(resultsample,
+                       'sample_%i_lr.png' % i, nrow=16)
+
+        save_pic(x_rec)
+
+
+def sample(cfg, logger):
+    torch.cuda.set_device(0)
+    model = Model(
+        startf=cfg.MODEL.START_CHANNEL_COUNT,
+        layer_count=cfg.MODEL.LAYER_COUNT,
+        maxf=cfg.MODEL.MAX_CHANNEL_COUNT,
+        latent_size=cfg.MODEL.LATENT_SPACE_SIZE,
+        truncation_psi=cfg.MODEL.TRUNCATIOM_PSI,
+        truncation_cutoff=cfg.MODEL.TRUNCATIOM_CUTOFF,
+        mapping_layers=cfg.MODEL.MAPPING_LAYERS,
+        channels=cfg.MODEL.CHANNELS,
+        generator=cfg.MODEL.GENERATOR,
+        encoder=cfg.MODEL.ENCODER)
+    model.cuda(0)
+    model.eval()
+    model.requires_grad_(False)
+
+    decoder = model.decoder
+    encoder = model.encoder
+    # mapping_tl = model.mapping_tl
+    mapping_fl = model.mapping_fl
+    dlatent_avg = model.dlatent_avg
+
+    logger.info("Trainable parameters generator:")
+    count_parameters(decoder)
+
+    logger.info("Trainable parameters discriminator:")
+    count_parameters(encoder)
+
+    arguments = dict()
+    arguments["iteration"] = 0
+
+    model_dict = {
+        #'discriminator_s': encoder,
+        'encoder_s': encoder,
+        'generator_s': decoder,
+        #'mapping_tl_s': mapping_tl,
+        'mapping_fl_s': mapping_fl,
+        'dlatent_avg': dlatent_avg
+    }
+
+    checkpointer = Checkpointer(cfg,
+                                model_dict,
+                                {},
+                                logger=logger,
+                                save=False)
+
+    extra_checkpoint_data = checkpointer.load()
+
+    model.eval()
+
+    layer_count = cfg.MODEL.LAYER_COUNT
+
+    def encode(x):
+        #Z, _ = model.encode(x, layer_count - 1, 1)
+        Z = model.encode(x, layer_count - 1, 1)
+        Z = Z.repeat(1, model.mapping_fl.num_layers, 1)
+        return Z
+
+    def decode(x):
+        layer_idx = torch.arange(2 * cfg.MODEL.LAYER_COUNT)[np.newaxis, :, np.newaxis]
+        ones = torch.ones(layer_idx.shape, dtype=torch.float32)
+        coefs = torch.where(layer_idx < model.truncation_cutoff, ones, ones)
+        # x = torch.lerp(model.dlatent_avg.buff.data, x, coefs)
+        #x = x[:,0]
+        return model.decoder(x, layer_count - 1, 1, noise=True)
+
+    rnd = np.random.RandomState(4)
+    latents = rnd.randn(1, cfg.MODEL.LATENT_SPACE_SIZE)
+
+    #path = 'realign1024x1024_'
+    #path = 'imagenet256x256'
+    path = 'realign128x128'
+
+    paths = list(os.listdir(path))
+
+    paths = sorted(paths)
+    random.seed(5)
+    random.shuffle(paths)
+
+    def make(paths):
+        canvas = []
+        with torch.no_grad():
+            for filename in paths:
+                img = np.asarray(Image.open(path + '/' + filename))
+                if img.shape[2] == 4:
+                    img = img[:, :, :3]
+                im = img.transpose((2, 0, 1))
+                x = torch.tensor(np.asarray(im, dtype=np.float32), device='cpu', requires_grad=True).cuda() / 127.5 - 1.
+                if x.shape[0] == 4:
+                    x = x[:3]
+                latents = encode(x[None, ...].cuda())
+                f = decode(latents)
+                #r = torch.cat([x[None, ...].detach().cpu(), f.detach().cpu()], dim=2)
+                canvas.append(f.detach().cpu())
+        return canvas
+
+    canvas = make(paths[:16])
+    canvas = torch.cat(canvas, dim=0)
+
+    save_image(canvas * 0.5 + 0.5, 'reconstructions_celeba_sep.png', nrow=16)
+
+
+if __name__ == "__main__":
+    gpu_count = 1
+    run(sample, get_cfg_defaults(), description='StyleGAN', default_config='configs/experiment_celeba_sep.yaml',
+        world_size=gpu_count, write_log=False)
