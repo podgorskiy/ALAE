3edb404a93c6a2d6776cdb68c02dcf36c4e5508e
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-16 15:15:05 -0400

    Added bedroom model

--- training_artifacts/download_all.py
+++ training_artifacts/download_all.py
@@ -2,3 +2,4 @@ import dlutils
 
 dlutils.download.from_google_drive('170Qldnn28IwnVm9CQEq1AZhVsK7PJ0Xz', directory='training_artifacts/ffhq')
 dlutils.download.from_google_drive('1T4gkE7-COHpX38qPwjMYO-xU-SrY_aT4', directory='training_artifacts/celeba')
+dlutils.download.from_google_drive('1gmYbc6Z8qJHJwICYDsB4aBMxXjnKeXA_', directory='training_artifacts/bedroom')
