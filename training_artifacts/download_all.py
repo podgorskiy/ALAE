421e664d404a38610cb25a6608b36d84c138b500
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-17 05:10:23 -0400

    Fixed celeba-hq configs

--- training_artifacts/download_all.py
+++ training_artifacts/download_all.py
@@ -3,4 +3,4 @@ import dlutils
 dlutils.download.from_google_drive('170Qldnn28IwnVm9CQEq1AZhVsK7PJ0Xz', directory='training_artifacts/ffhq')
 dlutils.download.from_google_drive('1T4gkE7-COHpX38qPwjMYO-xU-SrY_aT4', directory='training_artifacts/celeba')
 dlutils.download.from_google_drive('1gmYbc6Z8qJHJwICYDsB4aBMxXjnKeXA_', directory='training_artifacts/bedroom')
-dlutils.download.from_google_drive('12jDyMR8QyW_1suvMDE-v0ImruLaBl5k4', directory='training_artifacts/celeba-hq')
+dlutils.download.from_google_drive('12jDyMR8QyW_1suvMDE-v0ImruLaBl5k4', directory='training_artifacts/celeba-hq256')
