621bd4a5495358876c26d12d8aa246f59e792634
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-17 22:51:19 -0400

    Added link for models to download

--- training_artifacts/download_all.py
+++ training_artifacts/download_all.py
@@ -1,6 +1,9 @@
 import dlutils
 
 dlutils.download.from_google_drive('170Qldnn28IwnVm9CQEq1AZhVsK7PJ0Xz', directory='training_artifacts/ffhq')
+dlutils.download.from_google_drive('1QESywJW8N-g3n0Csy0clztuJV99g8pRm', directory='training_artifacts/ffhq')
+dlutils.download.from_google_drive('18BzFYKS3icFd1DQKKTeje7CKbEKXPVug', directory='training_artifacts/ffhq')
 dlutils.download.from_google_drive('1T4gkE7-COHpX38qPwjMYO-xU-SrY_aT4', directory='training_artifacts/celeba')
 dlutils.download.from_google_drive('1gmYbc6Z8qJHJwICYDsB4aBMxXjnKeXA_', directory='training_artifacts/bedroom')
-dlutils.download.from_google_drive('12jDyMR8QyW_1suvMDE-v0ImruLaBl5k4', directory='training_artifacts/celeba-hq256')
+dlutils.download.from_google_drive('1ihJvp8iJWcLxTIjkV5cyA7l9TrxlUPkG', directory='training_artifacts/celeba-hq256')
+dlutils.download.from_google_drive('1gFQsGCNKo-frzKmA3aCvx07ShRymRIKZ', directory='training_artifacts/celeba-hq256')
